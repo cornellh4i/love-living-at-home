718e38f2906803548b79087bdcaf090787f660be
NishatTasneem <nt264@cornell.edu>
2021-04-06 21:46:20 -0400

    Coded new fields into forms.py and the html file -Nat

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -17,6 +17,7 @@ from wtforms.validators import (
     EqualTo,
     InputRequired,
     Length,
+    DataRequired
 )
 
 from app import db
@@ -81,29 +82,33 @@ class NewUserForm(InviteUserForm):
 
 class SearchRequestForm(FlaskForm):
 
-    request_type = SelectMultipleField('Request Type', choices = [(0,'Check All'),
-    (1,'Transportation Request'), (2,'Member\'s Home Request'),
-    (3,'Contractor Referral'), (4,'Office Time Request')])
+    request_type = SelectMultipleField('Request Type', choices = [ (0,'Transportation Request'), 
+    (1,'Member\'s Home Request'), (2, 'Office Time Request')], validators = [DataRequired()])
 
 
     request_status = SelectMultipleField('Request Status', choices = 
-    [(0, 'Check All'), (1, 'Requested'), (2, 'Open Referral'), (3, 'Pending'),
-    (4, 'Confirmed'), (5, 'Completed'), (6, 'Cancelled')])
+    [(0, 'Requested'), (1, 'Confirmed'), (2, 'Completed'), (3, 'Cancelled')], validators = [DataRequired()])
 
     service_category = SelectMultipleField('Service Category', choices =
-    [(0, 'Check All'), (1, 'Coronavirus Community Support'), (2, 'Professional Home/Garden Service'),
-    (3, 'Professional In-Home Support'), (4, 'Technical Support'), 
-    (5, 'Transportation'), (6, 'Village Admin'), (7, 'Volunteer Home/Garden Service'),
-    (8, 'Volunteer In-Home Support')])
+    [(0, 'COVID Community Support'), (1, 'Professional Home/Garden Service'),
+    (2, 'Professional In-Home Support'), (3, 'Technical Support'), 
+    (4, 'Transportation'), (5, 'Village Admin'), (6, 'Volunteer Home/Garden Service'),
+    (7, 'Volunteer In-Home Support')], validators = [DataRequired()])
 
     provider_type = SelectMultipleField('Provider Type', choices = 
-    [(0, 'Check All'), (1, 'Non-Member Volunteer'), (2, 'Member Volunteer'), (3, 'Contractor')])
+    [(0, 'Non-Member Volunteer'), (1, 'Member Volunteer'), (2, 'Contractor')], validators = [DataRequired()])
 
-    service_req_from = IntegerField('Service Req # from', default=0)
+    requesting_member = StringField('Requesting Member', validators=[InputRequired(),
+                                  Length(1, 64)])
+
+    service_provider = StringField("Service Provider", validators=[InputRequired(),
+                                  Length(1, 64)])
+
+    """service_req_from = IntegerField('Service Req # from', default=0)
     service_req_to = IntegerField('to', default=0)
 
     priority = RadioField('High priority', choices=['Yes', 'No', 'Both'])
     show = RadioField('Show', choices=['Undated', 'Dated', 'Both'])
 
     search = SubmitField('Search')
-    reset = SubmitField('Reset')
+    reset = SubmitField('Reset')"""
