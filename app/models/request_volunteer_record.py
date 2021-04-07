023f42f518e30a5d0d9cc14afcb2ceb050973b61
acjin21 <angelacqjin@gmail.com>
2021-04-07 10:30:17 -0400

    rename staff_member_id to staffer_id

--- app/models/request_volunteer_record.py
+++ app/models/request_volunteer_record.py
@@ -10,7 +10,7 @@ class RequestVolunteerRecord(db.Model):
         nullable=False)
     status_id = db.Column(db.Integer, db.ForeignKey('status.id'),
         nullable=False)
-    staff_member_id = db.Column(db.Integer, db.ForeignKey('staffer.id'),
+    staffer_id = db.Column(db.Integer, db.ForeignKey('staffer.id'),
         nullable=False)
     updated_datetime = db.Column(db.DateTime, nullable = false)
 
