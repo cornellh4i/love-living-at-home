27d37aaae5c1c404505c1c3f58398f019b92715b
acjin21 <angelacqjin@gmail.com>
2021-04-07 12:02:23 -0400

    fix: db.Model instead of db.model

--- app/models/volunteer_type.py
+++ app/models/volunteer_type.py
@@ -1,7 +1,7 @@
 from flask import current_app
 from .. import db
 
-class VolunteerType(db.model):
+class VolunteerType(db.Model):
   __tablename__ = "volunteer_types"
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), nullable=False)
