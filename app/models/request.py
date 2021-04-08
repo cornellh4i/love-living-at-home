70814534e6ec0d2a7f49676ac12ab3acb9ea67f9
acjin21 <angelacqjin@gmail.com>
2021-04-07 20:56:28 -0400

    fix: add some comments and also remove some relationships

--- app/models/request.py
+++ app/models/request.py
@@ -7,36 +7,34 @@ from .. import db
 class Request(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   type_id = db.Column(db.Integer, db.ForeignKey('request_type.id'), nullable = False)
-  type = db.relationship("Type", backref="request", lazy = True)
   status_id = db.Column(db.Integer, db.ForeignKey('request_status.id'), nullable = False)
-  status = db.relationship("Status", backref="request", lazy = True)
+  short_description = db.Column(db.Text, nullable = False)
+  # Date Info
   created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().date())
+  modified_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().date())
   requested_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().date())
+  # Time Info
   initial_pickup_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().time())
   appointment_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().time())
   return_pickup_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().time())
   drop_off_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().time())
   is_date_time_flexible = db.Column(db.Boolean, nullable = False)
-  short_description = db.Column(db.Text, nullable = False)
-  service_category_id = db.Column(db.Integer, db.ForeignKey('service_category.id'), nullable = False)
-  service_category = db.relationship("Service Category", backref="request", lazy = True)
-  service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable = False)
-  service = db.relationship("Service", backref="request", lazy = True)
-  starting_address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable = False)
-  starting_address = db.relationship("Starting Address", backref="request", lazy = True)
-  destination_address_id = db.Column(db.Integer, db.ForeignKey('contact_method.id'), nullable = False)
-  destination_address = db.relationship("Destination Address", backref="request", lazy = True)
   duration_type_id = db.Column(db.Integer, db.ForeignKey('request_duration_type.id'), nullable = False)
-  duration_type = db.relationship("Duration Type", backref="request", lazy = True)
-  modified_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().date())
+  # Service Info
+  service_category_id = db.Column(db.Integer, db.ForeignKey('service_category.id'), nullable = False)
+  service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable = False)
+  # Location Info
+  starting_address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable = False)
+  destination_address_id = db.Column(db.Integer, db.ForeignKey('contact_method.id'), nullable = False)
+  # Member Info
   requesting_member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable = False)
-  requesting_member = db.relationship("Requesting Member", backref="request", lazy = True)
+  # Misc.
   special_instructions = db.Column(db.Text, nullable = False)
   followup_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().date())
+  # Staffer Info
   responsible_staffer_id = db.Column(db.Integer, db.ForeignKey('staffer.id'), nullable = False)
-  responsible_staffer = db.relationship("Responsible Staffer", backref="request", lazy = True)
+  # Contact Info
   contact_log_priority_id = db.Column(db.Integer, db.ForeignKey('contact_log_priority_type.id'), nullable = False)
-  contact_log_priority = db.relationship("Contact Log", backref="request", lazy = True)
   cc_email = db.Column(db.String(120), unique=True, nullable=False)
 
   def __repr__(self):
