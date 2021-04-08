15722b86abef64c8f69c88a1de9f586a91029ad6
acjin21 <angelacqjin@gmail.com>
2021-04-07 20:05:03 -0400

    fix: foreign key role.id should match table name

--- app/models/user.py
+++ app/models/user.py
@@ -52,7 +52,7 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
