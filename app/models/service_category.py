085eb1395e67dac187f88c21698f0b53a5888839
acjin21 <angelacqjin@gmail.com>
2021-04-10 08:09:59 -0400

    remove is_visible from ServiceCategory because all categories should be visible

--- app/models/service_category.py
+++ app/models/service_category.py
@@ -5,9 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 class ServiceCategory(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(), unique = True, nullable = False)
-    is_visible = db.Column(db.Boolean, nullable=False) 
     services = db.relationship("Service", backref = "service_category", lazy = True)
     requests = db.relationship("Request", backref = "service_category", lazy = True)
 
+    
     def __repr__(self):
-        return f"Service Category( '{self.name}', '{self.is_visible}')"
+        return f"Service Category('{self.name}')"
