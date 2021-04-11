a376d0c5c5d64b0e928d8e467620732bf3b25944
acjin21 <angelacqjin@gmail.com>
2021-04-11 09:40:20 -0400

    add comments to ProvidedService and remove volunteer_status field because not needed

--- app/models/provided_service.py
+++ app/models/provided_service.py
@@ -2,6 +2,9 @@ from .. import db
 
 
 class ProvidedService(db.Model):
+    """
+    Represents a single service provided by a single volunteer.
+    """
     id = db.Column(db.Integer, primary_key=True)
     service_id = db.Column(db.Integer,
                            db.ForeignKey('service.id'),
@@ -9,7 +12,6 @@ class ProvidedService(db.Model):
     volunteer_id = db.Column(db.Integer,
                              db.ForeignKey('volunteer.id'),
                              nullable=False)
-    volunteer_status = db.Column(db.String(80), nullable=False)
-
+                             
     def __repr__(self):
         return f"ProvidedService('{self.volunteer_status}')"
