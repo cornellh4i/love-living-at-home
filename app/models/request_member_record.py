4e40d9276ea0ba5984a4ab95c79020f93dfa633d
acjin21 <angelacqjin@gmail.com>
2021-04-15 13:22:34 -0400

    add metro area and request member record db tables

---
+++ app/models/request_member_record.py
@@ -0,0 +1,17 @@
+from .. import db
+
+
+class RequestMemberRecord(db.Model):
+    """
+    A record of a member who has requested a request. 
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    request_id = db.Column(db.Integer,
+                           db.ForeignKey('request.id'),
+                           nullable=False)
+    member_id = db.Column(db.Integer,
+                             db.ForeignKey('member.id'),
+                             nullable=False)
+    
+    def __repr__(self):
+        return f"RequestMemberRecord('Request ID: {self.request_id}, Member ID: {self.member_id}')"
